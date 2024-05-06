0e2ff2925179c8741c38d97e6adb7a25c2257a5b
Andrew Hsu <andrewhsu@MacBook-Air.local>
2024-05-06 14:35:13 -0700

    all of ship

--- player.h
+++ player.h
@@ -6,20 +6,17 @@
 class Player{
     private:
         string name;
-        Board* board;
         Ship* ships;
         int numShips;
     public:
         Player();
-        Player(string, Board*, Ship*, int);
+        Player(string, Ship*, int);
         Player(const Player&);
-        ~Player();
 
         Player& operator=(const Player&);
         friend ostream& operator<<(ostream&, Player&);
 
         string getName();
-        Board* getBoard();
         Ship* getShip();
         int getNumShips();
 
