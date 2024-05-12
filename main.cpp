a73837ac67947e464ea0bcd7b793ee496368932a
1106-Hsu-Andrew <ahsu2@cs202-ahsu2.lxd>
2024-05-12 23:47:35 +0000

    i made a board yipeeeee!

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 
 int main(){
     char** grid = new char*[10];
+    for(int i = 0; i < 10; i++){
+        grid[i] = new char[10];
+    }
     Board gameBoard(grid, 10, 10, 10, 10);
     displayBoard(gameBoard);
     // for(int k = 0; k < 9; k++){
